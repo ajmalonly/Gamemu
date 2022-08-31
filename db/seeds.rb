a6220da92a76c94bd7eef69549975c089aa87d61
thibault le maire <thibaultlemaire2004@gmail.com>
2022-08-31 11:46:57 +0400

    add dotenv-rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 puts "Cleaning Database"
 Booking.destroy_all
-User.destroy_all
 Game.destroy_all
+User.destroy_all
 puts "Database Clean"
 
 puts "Creating seeds"
@@ -12,13 +12,13 @@ user_three = User.create(email: "test3@gmail.com", password: "123456")
 game_one = Game.create(game_name: "Minecraft", description: "Build your world or try your survival skills", price: 100 , user: user_one)
 game_two = Game.create(game_name: "Grand Theft Auto V ", description: "Control 3 characters in one game", price: 250 , user: user_one)
 game_three = Game.create(game_name: "F1 2021", description: "Expose your drive skills", price: 150, user: user_one)
-game_four = Game.create(game_name: "Fifa 23", description: "Try to build the best team possible", price: 400, user: user_two)
+game_four = Game.create(game_name: "Fifa 22", description: "Try to build the best team possible", price: 125, user: user_two)
 game_five = Game.create(game_name: "Gods of War", description: "", price: 125, user: user_two)
 game_six = Game.create(game_name: "Watch dogs 2", description: "Hacking is the main passion", price: 125, user: user_two)
 game_seven = Game.create(game_name: "Assassins Creed Valhalla", description: "Explore and kill in silence", price: 200, user: user_three)
 game_height = Game.create(game_name: "The Crew 2", description: "Be the best driver in Chicago", price: 175, user: user_three )
 game_nine = Game.create(game_name: "Red Dead Redemption 2", description: "Be the most powerful cowboy", price: 300, user: user_three )
-game_ten = Game.create(game_name: "Playground Battleground", description: "Be the last survivor ", price: 75, user: user_three )
+game_ten = Game.create(game_name: "The Witcher 3", description: "Have the best bow and explore the world", price: 275, user: user_three )
 
 puts "Seeds created!"
 # This file should contain all the record creation needed to seed the database with its default values.
